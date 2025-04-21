4c20b1444cd7b53d5ece3c754799cbaeb5379035
ThomasSonnerstam <sonnerstam.thomas@hotmail.com>
2025-04-21 14:35:40 +0200

    Added table and initial styling
    - Added a table and navigation component for the data
    - Implemented some initial styling to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import MostWantedList from "./pages/MostWantedList";
+import { Title } from "@mantine/core";
+
 function App() {
   return (
-    <>
+    <div className="App">
+      <Title order={1}>FBI Most Wanted</Title>
       <MostWantedList />
-    </>
+    </div>
   );
 }
 
--- src/pages/MostWantedList.tsx
+++ src/pages/MostWantedList.tsx
@@ -1,17 +1,67 @@
 import { useState } from "react";
+import { Container, Group, Table, Text } from "@mantine/core";
+import { Pagination } from "@mui/material";
 import { useWantedPersons } from "../queries/useFbiData";
 
 const MostWantedList = () => {
   const [pageSize] = useState(20);
   const [page, setPage] = useState(1);
-  const { data, isLoading, error } = useWantedPersons(1, pageSize);
+  const { data, isLoading, error } = useWantedPersons(page, pageSize);
 
   console.log("data", data);
+  console.log("name", data?.items[5].title);
+
+  const tableRows = data?.items?.map((person) => (
+    <Table.Tr key={person.id}>
+      <Table.Td>{person.title}</Table.Td>
+      <Table.Td>{person.description}</Table.Td>
+      <Table.Td>{person.nationality}</Table.Td>
+      <Table.Td>{person.sex}</Table.Td>
+    </Table.Tr>
+  ));
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+  if (!data || !data.items) return <div>No data available</div>;
 
-  return <div>MostWantedList</div>;
+  const totalPages = Math.ceil((data.total || 0) / pageSize);
+
+  return (
+    <Container>
+      <Group justify="space-between" mt="md">
+        <Text size="sm">
+          Showing {data.items.length} of {data.total} results
+        </Text>
+        <Pagination
+          size="large"
+          color="secondary"
+          count={totalPages || 1}
+          page={page}
+          onChange={(_, newPage) => setPage(newPage)}
+          sx={{
+            "& .MuiPaginationItem-root": {
+              color: "white", // Text color for all pagination items
+            },
+            "& .MuiPaginationItem-page.Mui-selected": {
+              color: "white", // Text color for selected page (optional, already white by default)
+            },
+          }}
+        />
+      </Group>
+
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th align="left">Name</Table.Th>
+            <Table.Th align="left">Description</Table.Th>
+            <Table.Th align="left">Nationality</Table.Th>
+            <Table.Th align="left">Sex</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{tableRows}</Table.Tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default MostWantedList;
